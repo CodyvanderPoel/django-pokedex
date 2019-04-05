25ffec9cbfb99dd07369b0c4f3992abff9eb8b2a
Cody van der Poel <cvanderpoel@basecampcodingacademy.org>
2019-04-05 09:28:44 -0500

    Built my model

--- pokedex_app/models.py
+++ pokedex_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Pokemon(models.Model):
+    nationalId = models.IntegerField()
+    name = models.CharField(max_length=100)
+    pokemonType = models.CharField(max_length=100)
+    species = models.CharField(max_length=100)
+    height = models.CharField(max_length=100)
+    weight = models.CharField(max_length=100)
+    image = models.URLField()
+    def __str__(self):
+        return self.name
+    class Meta:
+        verbose_name_plural = "Pokemon"
