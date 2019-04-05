8018f122318b77e6224d3cf4f6eab42563d0d929
Cody van der Poel <cvanderpoel@basecampcodingacademy.org>
2019-04-05 09:30:13 -0500

    Setup the home view to access my database, and added the details page so you can view each pokemon individually

--- pokedex_app/views.py
+++ pokedex_app/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import render
 from django.views import View
+from .models import Pokemon
 
 # Create your views here.
 
 
 class Home(View):
     def get(self, request):
-        return render(request, 'home.html')
+
+        pokemon = Pokemon.objects.all().order_by("nationalId")
+
+        context = {
+            "pokemon": pokemon
+        }
+
+        return render(request, 'home.html', context)
+
+class Details(View):
+    def get(self, request ,id):
+
+        pokemon = Pokemon.objects.get(id=id)
+
+        context = {
+            "pokemon": pokemon
+        }
+
+        return render(request, 'details.html', context)
