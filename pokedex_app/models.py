27351c2156c71a9bf0a3c3cd5517c074337b1d4f
Cody van der Poel <cvanderpoel@basecampcodingacademy.org>
2019-04-09 11:32:09 -0500

    Added my Evolution model

--- pokedex_app/models.py
+++ pokedex_app/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 
+
 class Pokemon(models.Model):
     nationalId = models.IntegerField()
     name = models.CharField(max_length=100)
@@ -11,7 +12,22 @@ class Pokemon(models.Model):
     weight = models.CharField(max_length=100)
     image = models.URLField()
     desc = models.TextField()
+
     def __str__(self):
         return self.name
+
     class Meta:
         verbose_name_plural = "Pokemon"
+
+
+class Evolution(models.Model):
+    evolvesFrom = models.ForeignKey(Pokemon,
+                                    related_name="evolves_from_set",
+                                    on_delete=models.CASCADE)
+    evolvesInto = models.ForeignKey(Pokemon,
+                                    related_name="evolves_to_set",
+                                    on_delete=models.CASCADE)
+    requirements = models.TextField()
+
+    def __str__(self):
+        return f'{self.evolvesFrom.name} Evolution'
